a294dcdd5318457735fe991789fca5b3a2fb7afb
unknown <lucas-amstalden@hotmail.com>
2020-03-10 00:26:19 -0300

    Meu primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TesteGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aux = 0;
+            Console.WriteLine("Bem vindo, teste!");
+            Console.WriteLine("***************************");
+            Console.Write("Digite o número tabuada: ");
+            aux = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("O número digitado foi: " + aux.ToString());
+            Console.WriteLine("***************************");
+            
+            for(int i=1; i<11; i++)
+            {
+                Console.WriteLine(aux + " X " + i + " = " + (aux * i));
+            }
+        }
+    }
+}
